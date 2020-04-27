ad5b85cdd51e5b808392e41e7d9741c42d1d3920
Arthur Freitas <51511709+arthurfreitasrocha@users.noreply.github.com>
2020-04-27 18:27:41 -0300

    Update curtir_fotos_escolha__bancodedados.py

--- AutoLike v3.0/curtir_fotos_escolha__bancodedados.py
+++ AutoLike v3.0/curtir_fotos_escolha__bancodedados.py
@@ -5,14 +5,15 @@ from tkinter import messagebox
 # CUSTOM LIBRARY
 from curtir_fotos_escolha__bancodedados___abrirminibd import CurtirFotosEscolhaBancoDeDadosAbrirMiniBD
 from curtir_fotos_escolha__bancodedados___verselecionados import CurtirFotosEscolhaBancoDeDadosVerSelecionados
+from curtir_fotos_escolha__bancodedados___removerselecionados import CurtirFotosEscolhaBancoDeDadosRemoverSelecionados
 from curtir_fotos_escolha__bancodedados___acessarinstagram import CurtirFotosEscolhaBancoDeDadosAcessarInstagram
 
 
 class CurtirFotosEscolhaBancoDeDados():
 
-    def curtir_fotos(self, janela, quant_curtidas_usuarios):
+    def curtir_fotos(self, janela, quant_curtidas_usuarios, email_curto, status, n_usuarios):
 
-        if quant_curtidas_usuarios.isalpha == True:
+        if quant_curtidas_usuarios.isalpha == True or quant_curtidas_usuarios == '':
 
             messagebox.showinfo('Digite apenas números', 'Por favor informe um valor válido')
 
@@ -27,16 +28,55 @@ class CurtirFotosEscolhaBancoDeDados():
             conteudo = conteudo_file.split('-')
 
             a = CurtirFotosEscolhaBancoDeDadosAcessarInstagram(janela, conteudo, quant_curtidas_usuarios)
-
-    
-    
-    def ver_selecionados(self):
-
-        a = CurtirFotosEscolhaBancoDeDadosVerSelecionados()
+            janela.destroy()
+            a = CurtirFotosEscolhaBancoDeDados(email_curto, status, n_usuarios)
 
 
-    def abrir_mini_bd(self, email_curto):
+    # THIS FUNCTION CAN REMOVE ANY USER YOU HAVE
+    def remover_selecionados(self, janela, email_curto):
+
+        file = open('curtir_fotos_usuarios.txt', 'r')
+        conteudo_file = file.read()
+        file.close()
+
+        conteudo = conteudo_file.split('-')
+
+        if conteudo == [] or conteudo == '' or conteudo == ['']:
+
+            messagebox.showinfo('Você ainda não escolheu nenhum usuário', 'Por favor selecione pelo menos 1 usuário para continuar')
+
+        else:
+
+            janela.destroy()
+            a = CurtirFotosEscolhaBancoDeDadosRemoverSelecionados(email_curto)
+            resp = a.retorno()
+            a = CurtirFotosEscolhaBancoDeDados(resp[0], resp[1], resp[2])
         
+
+    # THIS FUNCTION SHOW THE USER YOU HAVE SELECTED
+    def ver_selecionados(self, janela, email_curto, status, n_usuarios):
+
+        file = open('curtir_fotos_usuarios.txt', 'r')
+        conteudo_file = file.read()
+        file.close()
+
+        conteudo = conteudo_file.split('-')
+
+        if conteudo == [] or conteudo == '' or conteudo == ['']:
+
+            messagebox.showinfo('Você ainda não escolheu nenhum usuário', 'Por favor selecione pelo menos 1 usuário para continuar')
+
+        else:
+
+            janela.destroy() 
+            a = CurtirFotosEscolhaBancoDeDadosVerSelecionados()
+            a = CurtirFotosEscolhaBancoDeDados(email_curto, status, n_usuarios)
+
+
+    # THIS FUNCTION SHOW THE LIST OF USERS IN THE DB
+    def abrir_mini_bd(self, janela, email_curto):
+        
+        janela.destroy()
         a = CurtirFotosEscolhaBancoDeDadosAbrirMiniBD(email_curto)
         resp = a.retorno()
         a = CurtirFotosEscolhaBancoDeDados(resp[0], resp[1], resp[2])
@@ -44,6 +84,21 @@ class CurtirFotosEscolhaBancoDeDados():
 
 
     def __init__(self, email_curto, status, n_usuarios):
+
+        try:
+
+            file = open('curtir_fotos_usuarios.txt', 'r')
+            conteudo = file.read()
+            file.close()
+
+            if conteudo == '':
+
+                status = 0
+
+        except:
+
+            pass
+
 
         janela = Tk()
 
@@ -56,7 +111,6 @@ class CurtirFotosEscolhaBancoDeDados():
 
         # VARIABLES
         n_usuarios = n_usuarios
-
         status = status
 
         # ADDITIONAL CONFIGURATION
@@ -67,10 +121,9 @@ class CurtirFotosEscolhaBancoDeDados():
 
             conteudo = conteudo_file.split('-')
 
-            if conteudo != [] and conteudo != '':
+            if conteudo != [] and conteudo != '' and conteudo != ['']:
 
                 n_usuarios = len(conteudo)
-
                 status = 1
 
         except:
@@ -98,7 +151,7 @@ class CurtirFotosEscolhaBancoDeDados():
         f_escolha = Frame(f_raiz, width=500, height=140, bg='floral white')
         f_escolha.pack(side=TOP)
         
-        if status == 1:
+        if status == 0:
             
             # LABEL CHOICE
             l_escolha = Label(f_escolha, text='Aqui aparecerão a quantidade de\nusuários selecionados',
@@ -115,19 +168,20 @@ class CurtirFotosEscolhaBancoDeDados():
         # BUTTON CHOICE
         b_escolha_remover_selecionado = Button(f_escolha, text='Remover\nSelecionados', font=('arial', 15, 'bold'),
         bg='dark salmon',
-        activebackground='salmon', activeforeground='white')
+        activebackground='salmon', activeforeground='white',
+        command=lambda: self.remover_selecionados(janela, email_curto))
         b_escolha_remover_selecionado.place(x=20, y=65)
 
         b_escolha_ver_selecionados = Button(f_escolha, text='Ver\nSelecionados', font=('arial', 15, 'bold'),
         bg='dark salmon',
         activebackground='salmon', activeforeground='white',
-        command=self.ver_selecionados)
+        command=lambda: self.ver_selecionados(janela, email_curto, status, n_usuarios))
         b_escolha_ver_selecionados.place(x=185, y=65)
 
         b_escolha = Button(f_escolha, text='Selecionar\nUsuários', font=('arial', 15, 'bold'),
         bg='dark salmon',
         activebackground='salmon', activeforeground='white',
-        command=lambda: self.abrir_mini_bd(email_curto))
+        command=lambda: self.abrir_mini_bd(janela, email_curto))
         b_escolha.place(x=350, y=65)
 
         # SEPARATOR ====================
@@ -153,7 +207,7 @@ class CurtirFotosEscolhaBancoDeDados():
         b_quant_curtidas = Button(f_quant_curtidas, text='ENVIAR', font=('arial', 15, 'bold'),
         bg='dark salmon',
         activebackground='salmon', activeforeground='white',
-        command=lambda: self.curtir_fotos(janela, e_quant_curtidas.get()))
+        command=lambda: self.curtir_fotos(janela, e_quant_curtidas.get(), email_curto, status, n_usuarios))
         b_quant_curtidas.place(x=280, y=90)
         
 
