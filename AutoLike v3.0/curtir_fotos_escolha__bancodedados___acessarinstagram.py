c56a0707930257830bcaf5e1cf29202208ab3c5f
Arthur Freitas <51511709+arthurfreitasrocha@users.noreply.github.com>
2020-04-27 22:35:40 -0300

    Update curtir_fotos_escolha__bancodedados___acessarinstagram.py

--- AutoLike v3.0/curtir_fotos_escolha__bancodedados___acessarinstagram.py
+++ AutoLike v3.0/curtir_fotos_escolha__bancodedados___acessarinstagram.py
@@ -12,9 +12,11 @@ from tkinter import messagebox
 import time
 import os
 
+
 class CurtirFotosEscolhaBancoDeDadosAcessarInstagram:
 
-    def remover_usuario(self, conteudo, indice):
+
+    def remover_usuario(self, conteudo_2, indice):
 
         if indice == -1:
             
@@ -23,24 +25,24 @@ class CurtirFotosEscolhaBancoDeDadosAcessarInstagram:
 
         else:
             
-            conteudo.pop(indice)
+            conteudo_2.pop(indice)
 
             # REMOVING USERS WITH ALREADY LIKED PHOTOS FROM THE LIST
             conteudo_string = ''
             i = 0
-            while(i < len(conteudo)):
+            while(i < len(conteudo_2)):
                         
                 if i == 0:
 
-                    conteudo_string = conteudo[i]
+                    conteudo_string = conteudo_2[i]
                         
                 else:
 
-                    conteudo_string += "-" + conteudo[i]
+                    conteudo_string += "-" + conteudo_2[i]
 
                 i += 1
 
-            if conteudo == []:
+            if conteudo_2 == []:
 
                 file = open('curtir_fotos_usuarios.txt', 'w')
                 file.close()
@@ -50,7 +52,6 @@ class CurtirFotosEscolhaBancoDeDadosAcessarInstagram:
                 file = open('curtir_fotos_usuarios.txt', 'w')
                 file.write(conteudo_string)
                 file.close()
-
 
 
     def __init__(self, janela, conteudo, quant_curtidas):
@@ -68,22 +69,23 @@ class CurtirFotosEscolhaBancoDeDadosAcessarInstagram:
         email = content[0]
         senha = content[1]
 
-
         # ACESSING INSTAGRAM
         options = Options()
         options.headless = False
-        
+
         driver = webdriver.Chrome(executable_path='C:\chromedriver.exe', chrome_options=options)
         driver.get('https://www.instagram.com/')
 
         driver.implicitly_wait(10)
-                
+
+        # WRITING THE MAIL AND PASSWORD ON THE BOXES
         driver.find_element(By.NAME, 'username').send_keys(email)
         driver.find_element(By.NAME, 'password').send_keys(senha)
         btn = driver.find_elements(By.TAG_NAME, 'button')
         btn[1].click()
         time.sleep(tempo)
 
+        # CLOSING THE BOX OF NOTIFICATIONS
         caixa_inicio = driver.find_elements(By.CLASS_NAME, 'aOOlW.HoLwm')
         caixa_inicio[0].click()
         time.sleep(tempo)
@@ -93,127 +95,116 @@ class CurtirFotosEscolhaBancoDeDadosAcessarInstagram:
         perfis = 0
         fotos = 0
         ultimo = 0
-        while(i <= len(conteudo)):
+        valor_conteudo = len(conteudo)
+        while(i <= valor_conteudo):
 
-            if i == len(conteudo):
+            if i == valor_conteudo-1:
 
                 ultimo = 1
 
-            if ultimo == 1:
-                
-                file = open('curtir_fotos_usuarios.txt', 'r')
-                conteudo = file.read()
-                file.close()
+
+            erro += 1
+
+            # ENTERING THE USERNAME IN THE INSTAGRAM TEXT BOX
+            j = 0
+            while(j < valor_conteudo):
+
+                if ultimo == 1:
+
+                    # TENTA INSERIR O ÚLTIMO ITEM DA LISTA
+                    try:
+                        
+                        inp = driver.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input')
+                        inp.send_keys(conteudo)
+                        break
+
+                    # SE NÃO CONSEGUIR, O LOOP É ENCERRADO
+                    except:
+
+                        break
+
+                else:
+
+                    # TENTA INSERIR O ITEM 'I' DA LISTA
+                    try:
+
+                        inp = driver.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input')
+                        inp.send_keys(conteudo[j])
+                        break
+
+                    # SE NÃO CONSEGUIR, O LOOP É ENCERRADO E O 'I' É INCREMENTADO
+                    except:
+                        
+                        i += 1
+                        break
+
+                j += 1
+
+            erro += 1
+
+            # CLICKING ON THE FIRST RETURN
+            nome = driver.find_elements(By.CLASS_NAME, 'z556c')
+            nome[0].click()
+
+            erro += 1
+
+            driver.implicitly_wait(10)
+            time.sleep(tempo)
+
+            # CLICKING ON THE FIRST PHOTO
+            foto = driver.find_elements(By.CLASS_NAME, 'v1Nh3.kIKUG._bz0w')
+            foto[0].click()
+
+            erro += 1
+
+            time.sleep(tempo)
+
+            # LIKE PHOTOS PROCESS
+            j = 0
+            print('PERFIL SENDO ACESSADO: {}'.format(conteudo[i]))
+            while(j < quant_curtidas):
+
+                coracao = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]/button')
+                coracao.click()   
+
+                seta_direita = driver.find_elements(By.CLASS_NAME, '_65Bje.coreSpriteRightPaginationArrow')
+                seta_direita[0].click()
+
+                time.sleep(tempo)
+
+                j += 1
+                fotos += 1
+
+                print('{} FOTO(S) CURTIDA(S) DE {}'.format(j, conteudo[i]))
+
+            print('====================')
+            erro += 1
+
+            time.sleep(tempo)
+
+            # CLOSE THE WINDOW OF PHOTOS
+            fechar = driver.find_element_by_xpath('/html/body/div[4]/div[3]/button')
+            fechar.click()
+
+            erro += 1
+
+            perfis += 1
+
+            if ultimo != 1:
+
+                self.remover_usuario(conteudo, i)
 
             else:
 
-                file = open('curtir_fotos_usuarios.txt', 'r')
-                conteudo_file = file.read()
-                file.close()
+                self.remover_usuario(conteudo, -1)
 
-                conteudo = conteudo_file.split('-')
+                # IF ALL GOES WELL A SUCCESS MESSAGE WILL BE SHOWN
+                messagebox.showinfo('Sucesso', 'Total de {} perfis acessados e {} fotos curtidas com sucesso!'.format(perfis, fotos))
 
-            try:
-                
-                time.sleep(tempo)
+                break
 
-                erro += 1
-
-                # ENTERING THE USERNAME IN THE INSTAGRAM TEXT BOX
-                if ultimo == 1:
-
-                    inp = driver.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input')
-                    inp.send_keys(conteudo)
-
-                else:
-
-                    inp = driver.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input')
-                    inp.send_keys(conteudo[i])
-
-
-                erro += 1
-
-                # CLICKING ON THE FIRST RETURN
-                nome = driver.find_elements(By.CLASS_NAME, 'z556c')
-                nome[0].click()
-
-                erro += 1
-
-                driver.implicitly_wait(1)
-                time.sleep(tempo+2)
-
-                # CLICKING ON THE FIRST PHOTO
-                foto = driver.find_elements(By.CLASS_NAME, 'v1Nh3.kIKUG._bz0w')
-                foto[0].click()
-
-                erro += 1
-
-                time.sleep(tempo)
-
-                # LIKE PHOTOS PROCESS
-                j = 0
-                print('PERFIL SENDO ACESSADO: {}'.format(conteudo[i]))
-                while(j < quant_curtidas):
-
-                    coracao = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]/button')
-                    coracao.click()   
-
-                    seta_direita = driver.find_elements(By.CLASS_NAME, '_65Bje.coreSpriteRightPaginationArrow')
-                    seta_direita[0].click()
-
-                    time.sleep(tempo)
-
-                    j += 1
-                    fotos += 1
-
-                    print('{} FOTOS CURTIDAS DE {}'.format(j, conteudo[i]))
-
-                print('====================')
-                erro += 1
-
-                time.sleep(tempo)
-
-                # CLOSE THE WINDOW OF PHOTOS
-                fechar = driver.find_element_by_xpath('/html/body/div[4]/div[3]/button')
-                fechar.click()
-
-                erro += 1
-
-                perfis += 1
-
-                if ultimo != 1:
-
-                    self.remover_usuario(conteudo, i)
-
-                else:
-
-                    self.remover_usuario(conteudo, -1)
-
-                    # IF ALL GOES WELL A SUCCESS MESSAGE WILL BE SHOWN
-                    messagebox.showinfo('Sucesso', 'Total de {} perfis acessados e {} fotos curtidas com sucesso!'.format(perfis, fotos))
-
-                    break
-
-
-            except:
-                
-                self.remover_usuario(conteudo, i)
-
-                # IF ANY ERROR HAS OCURRED, A MESSAGE WILL BE SHOWN
-                messagebox.showinfo('Erro', 'Algo deu errado e não foi possível concluir esta ação.\nTotal de {} perfis acessados e {} fotos curtidas. ERRO: 00{}'.format(perfis, fotos, erro))
-
-                erro = 1
 
             erro = 0
-            i += 1
-
-        #if erro < 1:
-            
-            #self.remover_usuario(conteudo, -1)
-
-            # IF ALL GOES WELL A SUCCESS MESSAGE WILL BE SHOWN
-            #messagebox.showinfo('Sucesso', 'Total de {} perfis acessados e {} fotos curtidas com sucesso!'.format(perfis, fotos))
+            valor_conteudo -= 1
 
         driver.close()
-
-#a = CurtirFotosEscolhaBancoDeDadosAcessarInstagram(['arthur2k_'], 3)
