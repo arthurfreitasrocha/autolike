67cc52f07338ec57405fb812aa3c10e8f9269ee3
Arthur Freitas <arthur.freitas2000@hotmail.com>
2020-11-17 11:16:32 -0300

    Update

--- AutoLike_4.2/view/Tela_Inicio.py
+++ AutoLike_4.2/view/Tela_Inicio.py
@@ -1,6 +1,24 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 class TelaInicio:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+
+        self.__gerarInterface()
+
+
+    def __gerarInterface(self):
+
+        janela = Tk()
+
+        frame = Frame(janela, width=900, height=100, bg="salmon").pack(side=TOP)
+        Label(frame, text="AutoLike v.4.2", font=("arial", 20, "bold")).pack(side=TOP)
+
+        janela.geometry("900x500+500+200")
+        janela.mainloop()
+
+
+if __name__ == "__main__":
+
+    interface = TelaInicio()
